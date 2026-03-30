e1061b8e7306627b4e9ef43da81672a7ede60f9b
LYS <ltltl725@gmail.com>
2026-03-31 02:12:51 +0900

    template method 과제

--- src/hw/ch03/Main.java
+++ src/hw/ch03/Main.java
@@ -5,21 +5,21 @@ public class Main {
 
         System.out.println("컴퓨터공학전공 이예서");
 
+        // [3-1] 기본 테스트
         AbstractDisplay d1 = new CharDisplay('H');
         AbstractDisplay d2 = new StringDisplay("Hello");
         d1.display();
         d2.display();
 
         // [3-2] Step 1 테스트: 반복 횟수 제어 
-        
-        AbstractDisplay d3 = new CharDisplay('X', 3);
-        AbstractDisplay d4 = new StringDisplay("Test", 7);
-        d3.display();
-        d4.display();
+        // AbstractDisplay d1 = new CharDisplay('X', 3);
+        // AbstractDisplay d2 = new StringDisplay("Test", 7);
+        // d1.display();
+        // d2.display();
 
         // [3-3] Step 2 테스트: 새로운 Display
-        AbstractDisplay num = new NumberDisplay(42, 4);
-        num.display();
+        // AbstractDisplay num = new NumberDisplay(42, 4);
+        // num.display();
 
     }
 }
