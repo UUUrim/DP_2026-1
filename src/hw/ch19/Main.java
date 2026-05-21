5d28a37ae516d21006727e5974bf47382b061847
LYS <ltltl725@gmail.com>
2026-05-21 11:06:16 +0900

    state 과제

---
+++ src/hw/ch19/Main.java
@@ -0,0 +1,18 @@
+package hw.ch19;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("컴퓨터공학전공 이예서");
+
+        SafeFrame frame = new SafeFrame("State Sample");
+        while (true) {
+            for (int hour = 0; hour < 24; hour++) {
+                frame.setClock(hour);   // 시간 설정 
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+    }
+}
